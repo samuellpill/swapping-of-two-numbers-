bcb279e22d850dbc22b4087d8f54e6c7bde85e5e
samuellpill <82765663+samuellpill@users.noreply.github.com>
2021-04-18 20:17:19 +0530

    swapping.c
    the file shows

---
+++ swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("enter the first number:");
+scanf("%d",&a0;
+printf("enter the second number:");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping: a=%d & b=%d",a,b) 
+}
